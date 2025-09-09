cde4a306172a7b2df77af3266157b3f04c594e61
spckEditor <myspck@mahmud.com>
2025-09-09 12:15:34 +0100

    basic html sha I wan rest

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-console.log("working js")
+const selectEl = document.querySelector("#yearSelect")
+let years = []
+
+let currentYear = new Date
+const NumberYear = currentYear.getFullYear() - 2000
+
+for (var i = 0; i <=NumberYear ; i++) {
+  if (i<=9) {
+    years.push(`200${i}`)
+  } else {
+    years.push(`20${i}`)
+  }
+  
+}
