d000340f73355151b7a27f954e887025f7db9537
spckEditor <myspck@mahmud.com>
2025-09-09 16:02:52 +0100

    the first html boiler plates for the gallery page still no major  styling yet

--- js/app.js
+++ js/app.js
@@ -1,14 +1,40 @@
-const selectEl = document.querySelector("#yearSelect")
+document.addEventListener("DOMContentLoaded", function () {
+  
+  
+  
+let selectEl= document.querySelector("select")
 let years = []
-
+let OptionList = []
 let currentYear = new Date
 const NumberYear = currentYear.getFullYear() - 2000
 
 for (var i = 0; i <=NumberYear ; i++) {
-  if (i<=9) {
-    years.push(`200${i}`)
-  } else {
-    years.push(`20${i}`)
-  }
+  years.push(2000 + i)
   
 }
+
+years.forEach((year)=>{
+  OptionList.push(`<option value="${year}">${year}</option>`)
+})
+
+OptionList.reverse()
+
+const joinedOptions = OptionList.join(' ');
+
+selectEl.innerHTML = joinedOptions;
+
+
+
+
+const imageGrid = document.getElementById('GallerywrapperGrid');
+
+        for (let i = 1; i <= 12; i++) {
+            const div = document.createElement('div');
+            div.className = 'gridImageItem';
+            const img = document.createElement('img');
+            img.src = `https://picsum.photos/200/200?random=${i}`; // Random image from Lorem Picsum
+            img.alt = `Image ${i}`;
+            div.appendChild(img);
+            imageGrid.appendChild(div);
+        }
+})
